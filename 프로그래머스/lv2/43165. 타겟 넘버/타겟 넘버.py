a39d72bd4d0ab7ce47cf806024addb54d136dde6
shinddong <98038384+shinddong@users.noreply.github.com>
2023-10-07 01:07:25 +0900

    [level 2] Title: 타겟 넘버, Time: 4.47 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -2,17 +2,32 @@ from collections import deque
 
 
 def solution(numbers, target):
-    answer = 0
+    def bfs(x,y):
+        answer = 0
 
-    queue = deque()
-    queue.append((0, 0))
-    while queue:
-        x, y = queue.popleft()
-        if y == len(numbers) :
-            if x==target:
-                answer+=1
-            else: continue
-        else:
-          queue.append((x+numbers[y], y+1))
-          queue.append((x-numbers[y], y+1))
-    return answer
+        queue=deque()
+        queue.append((x,y))
+        while queue:
+            x,y =queue.popleft()
+            if y==len(numbers):
+                if x==target:
+                    answer+=1
+                else: continue
+            else:
+                queue.append((x+numbers[y],y+1))
+                queue.append((x-numbers[y],y+1))
+        return answer
+    return bfs(0,0)
+   
+    # queue = deque()
+    # queue.append((0, 0))
+    # while queue:
+    #     x, y = queue.popleft()
+    #     if y == len(numbers) :
+    #         if x==target:
+    #             answer+=1
+    #         else: continue
+    #     else:
+    #       queue.append((x+numbers[y], y+1))
+    #       queue.append((x-numbers[y], y+1))
+    # return answer
